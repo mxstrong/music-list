a6855cfd1550dfd877cfd33e7decf99c1c8687fc
Mantas Ptakauskas <mantas.ptakauskas@visma.com>
2022-11-09 10:52:37 +0200

    initialise project

---
+++ src/main/java/com/example/musiclist/MusicListApplication.java
@@ -0,0 +1,13 @@
+package com.example.musiclist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicListApplication.class, args);
+    }
+
+}
